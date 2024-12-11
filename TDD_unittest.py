6642f3a6eebac8323ae11dd6fa26ba3c6ebf635e
sooyeon0301 <leesooy0301@gmail.com>
2024-12-11 19:35:22 +0900

    Update TDD_unittest.py

--- TDD_unittest.py
+++ TDD_unittest.py
@@ -18,6 +18,14 @@ class TDDUnittest(unittest.TestCase):
         self.assertEqual(self.car_controller.get_right_door_status(), "CLOSED")
         self.assertEqual(self.car_controller.get_speed() , 0)
 
+    def test_engine_brake(self):
+        self.car_controller.unlock_vehicle()  # 차량 전체 잠금 해제
+        self.car_controller.toggle_engine()  # 엔진 ON
+        self.car_controller.brake() # 브레이크
+
+        execute_command_callback("ENGINE_BIN BRAKE", self.car_controller)
+        self.assertEqual(self.car_controller.get_engine_status(), False)
+
 
 if __name__ == "__main__":
     unittest.main(exit = False)
