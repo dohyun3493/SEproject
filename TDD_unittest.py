d2424202ca832ca1c45a20418bc12f675fd8c370
IwantTopProGodKTY <taeyeon5332974@gmail.com>
2024-12-11 19:34:05 +0900

    5번 테스트함수 추가

--- TDD_unittest.py
+++ TDD_unittest.py
@@ -5,10 +5,19 @@ from main import execute_command_callback
 
 
 class TDDUnittest(unittest.TestCase):
-    def setUp(self):
-        self.car = Car()
-        self.car_controller = CarController(self.car)
+    car = Car()
+    car_controller = CarController(car)
+        
     
+    def testSet_five(self):
+        self.car_controller.unlock_vehicle()
+        
+        execute_command_callback("LEFT_DOOR_OPEN ACCELERATE BRAKE RIGHT_DOOR_OPEN BRAKE ACCELERATE BRAKE BREAK ENGIN_BIN",self.car_controller)
+        self.assertEqual(self.car_controller.get_engine_status(), True)
+        self.assertEqual(self.car_controller.get_left_door_status(), "CLOSED")
+        self.assertEqual(self.car_controller.get_right_door_status(), "CLOSED")
+        self.assertEqual(self.car_controller.get_speed() , 0)
+
 
 if __name__ == "__main__":
     unittest.main(exit = False)
