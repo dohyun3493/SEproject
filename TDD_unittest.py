28c9a35638cb2503e729fc561004deaeffb60913
sooyeon0301 <leesooy0301@gmail.com>
2024-12-11 19:36:28 +0900

    Update TDD_unittest.py

--- TDD_unittest.py
+++ TDD_unittest.py
@@ -12,7 +12,7 @@ class TDDUnittest(unittest.TestCase):
     def testSet_five(self):
         self.car_controller.unlock_vehicle()
         
-        execute_command_callback("LEFT_DOOR_OPEN ACCELERATE BRAKE RIGHT_DOOR_OPEN BRAKE ACCELERATE BRAKE BREAK ENGIN_BIN",self.car_controller)
+        execute_command_callback("LEFT_DOOR_OPEN ACCELERATE BRAKE RIGHT_DOOR_OPEN BRAKE ACCELERATE BRAKE BRAKE ENGINE_BIN",self.car_controller)
         self.assertEqual(self.car_controller.get_engine_status(), True)
         self.assertEqual(self.car_controller.get_left_door_status(), "CLOSED")
         self.assertEqual(self.car_controller.get_right_door_status(), "CLOSED")
